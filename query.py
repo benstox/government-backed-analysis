d010ae8c00d07c6a04bad4ac3b175c07d1fdfa86
benstox <benstox@gmail.com>
2017-04-07 16:05:32 +0100

    Added an alternative stepwise model selector in that doesn't use AIC.

--- query.py
+++ query.py
@@ -7,7 +7,23 @@ from fdbapp.models import Fund, Fundraising, InvestmentType, RegionGroup, Sector
 year_start = timezone.datetime.strptime("2011-01-01", "%Y-%m-%d")
 year_end = timezone.datetime.strptime("2011-12-31", "%Y-%m-%d")
 
+# fundraising export
 # ?investee__company__country__id__exact=gb&primary_date__gte=2011-01-01&primary_date__lte=2011-12-31&published__exact=1&investment_type__id=7
+# fields to download:
+# - ID (id)
+# - investee (investee)
+# - investee_id (investee_id)
+# - primary date (primary_date)
+# - amount coalesced currency (amount_coalesced_currency)
+# - amount coalesced value (amount_coalesced_value)
+# - stage of evolution (stage_of_evolution)
+# - participating funds fund types (_participating_funds_fund_types)
+# - participating funds (_participating_funds)
+# - sectors (investee__sectors)
+# - companies house id (investee__company__companies_house_id)
+# - head office local authority (investee__company__head_office_local_authority)
+# - Current stage of evolution (investee__company__current_stage_of_evolution)
+# - Value of one unit in pounds sterling according to today's exchange rates (amount_coalesced_currency__value)
 fs = Fundraising.objects.filter(
     investee__company__country__id__exact="gb",
     primary_date__gte=year_start, primary_date__lte=year_end,
